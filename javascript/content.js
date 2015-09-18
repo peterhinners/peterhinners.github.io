6eba82e4e95b0047b6ece8bdcbaa58893a257e8b
peterhinners <peterhinners@gmail.com>
2015-09-18 12:56:36 -0500

    ajax

--- javascript/content.js
+++ javascript/content.js
@@ -1,7 +1,8 @@
-$(function() {
+$(window).load(function() {
 	console.log("hungry");
   $(".entries").on("click", "a", function(event){
   	var link = $(this).attr("href");
+  	console.log(link);
   	// event.preventDefault();
   	$(".entries").hide();
     var request = $.ajax({url: link});
