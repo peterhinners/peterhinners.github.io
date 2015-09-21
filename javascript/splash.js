1cf6e64a1972589bd0d30bd4dd5c081e083a5534
peterhinners <peterhinners@gmail.com>
2015-09-21 16:54:42 -0500

    sound

--- javascript/splash.js
+++ javascript/splash.js
@@ -1,40 +1,26 @@
-
-
 $(function() {
   $('.navbar').load('../navbar.html');
   $('#landing').load('../landing.html');
 });
 
 $(window).load(function() {
-	console.log("ready for break");
+	console.log("time for break");
   $(".navbar").on("click", "a", function(event){
   	event.preventDefault();
-  	
-  	if (typeof pauseMusic == 'function') { 
-  		// requestID = requestAnimationFrame(main); 
-  		// $(window).cancelAnimationFrame(requestID); 
-
+  	if(typeof pauseMusic == 'function') { 
   		pauseMusic();
   		pauseToons();
 			}
-
-
   	$("#landing").hide();
   	$("#canvas").remove();
-  	
-  	
-  	
   	var link = $(this).attr("href");
   	var request = $.ajax({url: link});
   	request.done(function(response){
 	  	if(link == "http://peterhinners.github.io"){
 	  		$(".main-content").html("");
 	  		$("#landing").show();
-	  		
-	  		
 	  	} else {
 	  		$(".main-content").html(response);
-	  		
 	  	}
   	});
 	});
@@ -42,30 +28,14 @@ $(window).load(function() {
 	$(".main-content").on("click", "a", function(event){
 		var link = $(this).attr("href");
 		if(link == "http://catbreak.herokuapp.com"){
-			// } else if(link == "http://peterhinners.github.io/blog/TomJerry.html"){
-			// event.preventDefault();
-			// $(".navbar").hide();
-			// // $(".main-content").html("");
-			// var request = $.ajax({url: link});
-			//   request.done(function(response){
-			//   	// $(".navbar").hide();
-			//   	$(".main-content").html(response);
-			//   });
 			}	else {
 			event.preventDefault();
-			$(".main-content").html("");
 			var request = $.ajax({url: link});
 			  request.done(function(response){
-			  	
 			  	$(".main-content").html(response);
 			  });
-		}
-		
+		}	
 	});
-  	
-  	
-
-
 });
 
 window.addEventListener("keydown", function(e) {
