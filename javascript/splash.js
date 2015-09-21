bbb1ef6df3ed6528de03108e34b7df30c73c2a03
peterhinners <peterhinners@gmail.com>
2015-09-21 11:06:52 -0500

    ajax

--- javascript/splash.js
+++ javascript/splash.js
@@ -5,11 +5,10 @@ $(function() {
 });
 
 $(window).load(function() {
-	console.log("colon");
+	console.log("purple");
   $(".navbar").on("click", "a", function(event){
   	event.preventDefault();
   	$("#landing").hide();
-  	$(".main-content").html("");
   	$("#canvas").remove();
   	var link = $(this).attr("href");
   	var request = $.ajax({url: link});
@@ -26,14 +25,15 @@ $(window).load(function() {
 
 	$(".main-content").on("click", "a", function(event){
 		var link = $(this).attr("href");
-		if(link == "http://peterhinners.github.io/blog/TomJerry.html"){
+		if(link == "http://catbreak.herokuapp.com"){
+			} else {
 			event.preventDefault();
 			$(".main-content").html("");
 			var request = $.ajax({url: link});
 			  request.done(function(response){
 			  	$(".main-content").html(response);
 			  });
-		} else {};
+		}
 		
 	});
   	
