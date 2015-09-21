a4a44be9e8ae547fd278acf48044750404b0c851
peterhinners <peterhinners@gmail.com>
2015-09-21 09:28:19 -0500

    nav

--- javascript/splash.js
+++ javascript/splash.js
@@ -1,6 +1,7 @@
 $(function() {
   $('.navbar').load('../navbar.html');
   $('#landing').load('../landing.html');
+  
 });
 
 $(window).load(function() {
@@ -9,11 +10,13 @@ $(window).load(function() {
   	var link = $(this).attr("href");
   	console.log(link);
   	event.preventDefault();
-  	$("#landing").toggle();
+  	$("#landing").hide();
+  	$(".main-content").hide();
     var request = $.ajax({url: link});
     request.done(function(response){
     	console.log(response);
     	$(".main-content").append(response);
+    	$(".main-content").show();
     });
 	});
 });
