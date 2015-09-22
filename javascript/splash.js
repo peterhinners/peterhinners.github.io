98d7d981c7268ec1068bd10263853f4dcded916d
peterhinners <peterhinners@gmail.com>
2015-09-22 16:14:10 -0500

    more css

--- javascript/splash.js
+++ javascript/splash.js
@@ -6,7 +6,7 @@ $(function() {
 });
 
 $(window).load(function() {
-	console.log("css2");
+	console.log("css3");
   $(".navbar").on("click", "a", function(event){
   	event.preventDefault();
   	if(typeof pauseMusic == 'function') { 
