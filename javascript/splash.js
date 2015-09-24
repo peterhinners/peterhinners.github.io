2720d287cfde1d6b8ccf0709520008b1e83a6849
peterhinners <peterhinners@gmail.com>
2015-09-24 13:13:02 -0500

    css normalizer

--- javascript/splash.js
+++ javascript/splash.js
@@ -8,7 +8,7 @@
 // $(window).load(function() {
 
 $(function() {	
-	console.log("weird");
+	console.log("pad");
 
 	$('.navbar').load('../navbar.html');
   $('.main-content').load('../landing.html');
