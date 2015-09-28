9626fd37e2e9a5595ea0f2542e47d097f1d5fa26
peterhinners <peterhinners@gmail.com>
2015-09-28 12:01:08 -0500

    contact

--- javascript/splash.js
+++ javascript/splash.js
@@ -8,7 +8,7 @@
 // $(window).load(function() {
 
 $(function() {	
-	console.log("linkedin5");
+	console.log("linkedin6");
 
 	$('.navbar').load('../navbar.html');
   $('.main-content').load('../landing.html');
