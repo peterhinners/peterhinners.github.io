541bc1b204539571cb68e2481654584b62a6388b
peterhinners <peterhinners@gmail.com>
2015-09-28 11:51:33 -0500

    contact

--- javascript/splash.js
+++ javascript/splash.js
@@ -8,7 +8,7 @@
 // $(window).load(function() {
 
 $(function() {	
-	console.log("linkedin2");
+	console.log("linkedin3");
 
 	$('.navbar').load('../navbar.html');
   $('.main-content').load('../landing.html');
