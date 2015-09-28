56c73d735e6331ae7acedc21b356cac0da3b9ef7
peterhinners <peterhinners@gmail.com>
2015-09-28 14:02:44 -0500

    footer

--- javascript/splash.js
+++ javascript/splash.js
@@ -8,7 +8,7 @@
 // $(window).load(function() {
 
 $(function() {	
-	console.log("nav");
+	console.log("footer");
 
 	$('.navbar').load('../navbar.html');
   $('.main-content').load('../landing.html');
